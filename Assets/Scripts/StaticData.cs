7a1fa151bfc0a3f7e3c31820731c3036d829e8d6
hjzre <142443853+hjzres@users.noreply.github.com>
2023-11-29 19:36:27 -0500

    Added "StaticData" script + added some values in it

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    // Setting values
+    public static float sensitivity = 50f;
+    public static float volume = 100f;
+
+    // Scenes
+    public static int mainMenuScene = 0;
+    public static int levelsScene = 1;
+}
